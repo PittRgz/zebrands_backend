22fefa3963549be5f1c8c932f79ae9e27f1a6dde
Pedro Cesar Rodriguez Medina <pedro.cesar.rodriguez.medina@ibm.com>
2021-06-03 11:59:54 -0500

    Removing a comment

--- app/products/urls.py
+++ app/products/urls.py
@@ -5,7 +5,6 @@ from products import views
 app_name = 'products'
 
 urlpatterns = [
-    # path('', views.ProductViewSet, name='list'),
     path('', views.ProductViewSet.as_view({'get': 'list'}), name='list'),
     path('create/', views.CreateProductView.as_view(), name='create'),
     path('manage/<int:product_id>/', views.ManageProductView.as_view(), name='product'),
