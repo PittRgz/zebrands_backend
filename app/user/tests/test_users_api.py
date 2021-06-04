d3e8e8688f4d769249c53537adf99b578299cdec
Pedro Cesar Rodriguez Medina <pedro.cesar.rodriguez.medina@ibm.com>
2021-06-04 00:02:13 -0500

    Added a users view set to list all users along with its unit tests

--- app/user/tests/test_users_api.py
+++ app/user/tests/test_users_api.py
@@ -5,8 +5,11 @@ from django.urls import reverse
 from rest_framework.test import APIClient   # To make requests to our API
 from rest_framework import status
 
+from user.serializers import UserSerializer
+
 CREATE_USER_URL = reverse('user:create')  # User create URL constant
 TOKEN_URL = reverse('user:token')  # User token URL constant
+LIST_USERS_URL = reverse('user:list')  # User list URL constant
 
 
 def unique_user_url(user_id=1):
@@ -25,6 +28,16 @@ class PublicUsersAPITests(TestCase):
     def setUp(self):
         """Initialize Client"""
         self.client = APIClient()
+
+    def test_retrieve_users_fails_when_unauthorized(self):
+        """Test that list users fails when user is not authenticated"""
+        # First populate the DB with some dummy users
+        create_user(email='user1@zebrands.com', password='p111', name='User One')
+        create_user(email='user2@zebrands.com', password='p222', name='User Two')
+
+        result = self.client.get(LIST_USERS_URL)
+
+        self.assertEqual(result.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_user_fails_when_user_unauthorized(self):
         """Test creating a user already exists fails"""
@@ -58,6 +71,20 @@ class PrivateUserAPITests(TestCase):
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_users_success(self):
+        """Test that all users are retrieved for authenticated users"""
+        # First populate the DB with some dummy users
+        create_user(email='user1@zebrands.com', password='p111', name='User One')
+        create_user(email='user2@zebrands.com', password='p222', name='User Two')
+
+        result = self.client.get(LIST_USERS_URL)
+
+        users = get_user_model().objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.data, serializer.data)
 
     def test_create_valid_user_success(self):
         """Test creating user with valid data is successful"""
@@ -142,12 +169,13 @@ class PrivateUserAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_profile_success(self):
-        """Test retrieving profile for existent in user"""
+        """Test retrieving profile for existent user"""
         url = unique_user_url(self.user.id)
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
+            'id': self.user.id,
             'name': self.user.name,
             'email': self.user.email
         })
